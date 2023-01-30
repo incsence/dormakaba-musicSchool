ffca8d9c46b0ea4cd2f4188387860b654bb9d9dd
incsence <57865808+incsence@users.noreply.github.com>
2023-01-30 17:16:46 +0100

    Add files via upload
    newest MusicSchool.jar and MusicSchool.bat

---
+++ MusicSchool.bat
@@ -0,0 +1,2 @@
+:: java -cp .\MusicSchool.jar Main
+"C:\Program Files\Java\jdk-17.0.5\bin\java.exe" -Dfile.encoding=UTF-8 -classpath "DormakabaMusicSchool Production;lib\mssql-jdbc-11.2.1.jre17.jar" Main
